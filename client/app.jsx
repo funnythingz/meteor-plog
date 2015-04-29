0aa50813cc6926e53100e3108721eba919ec0efa
funnythingz <funnythingz69+github@gmail.com>
2015-04-29 11:44:26 +0900

    added post button

--- client/app.jsx
+++ client/app.jsx
@@ -8,10 +8,16 @@ var New = ReactMeteor.createClass({
             <div className="row">
                 <form className="col s12">
                     <div className="row">
-                        <div className="input-field col s12">
+                        <div className="input-field col s10">
                             <i className="mdi-editor-mode-edit prefix"></i>
-                            <textarea id="iro_content" className="materialize-textarea"></textarea>
+                            <textarea id="iro_content" className="materialize-textarea" length="120"></textarea>
                             <label for="iro_content">Message</label>
+                        </div>
+                        <div className="input-field col s2">
+                            <div className="btn waves-effect waves-light">
+                                Post
+                                <i className="mdi-content-send right"></i>
+                            </div>
                         </div>
                     </div>
                 </form>
