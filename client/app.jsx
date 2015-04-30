781be0a6fcca6277424992f9b745bdd80834608a
funnythingz <funnythingz69+github@gmail.com>
2015-05-01 00:30:04 +0900

    can not use ES6

--- client/app.jsx
+++ client/app.jsx
@@ -1,23 +1,21 @@
 const New = ReactMeteor.createClass({
     templateName: "New",
 
-    handleSubmit: e => {
-        var content = React.findDOMNode(this.refs.iroContent).value.trim()
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var content = React.findDOMNode(this.refs.iroContent).value.trim();
         Iros.insert({ content: content, createdAt: new Date()});
+        React.findDOMNode(this.refs.iroContent).clear();
     },
 
-    results: () => {
-        return Iros.find({}, {sort: {createdAt: -1}});
-    },
-
-    render: () => {
+    render: function() {
         return (
             <div className="row">
                 <form className="col s12" onSubmit={this.handleSubmit}>
                     <div className="row">
                         <div className="input-field col s10">
                             <i className="mdi-editor-mode-edit prefix"></i>
-                            <textarea id="iro_content" className="materialize-textarea" length="120" ref="iroContent"></textarea>
+                            <textarea id="iro_content" className="materialize-textarea" length="120" ref="iroContent" />
                             <label for="iro_content">Message</label>
                         </div>
                         <div className="input-field col s2">
@@ -36,9 +34,24 @@ const New = ReactMeteor.createClass({
 const IroIro = ReactMeteor.createClass({
     templateName: "IroIro",
 
-    render: () => {
+    getMeteorState: function() {
+        return {
+            iroiro: Iros.find({}, {sort: {createdAt: -1}}).fetch()
+        }
+    },
+
+    render: function() {
+        var iroiro = this.state.iroiro.map(function(iro) {
+            return (
+                <p id={iro._id}>
+                    {iro.content}
+                </p>
+            );
+        });
+
         return (
             <section className="iroiro">
+                {iroiro}
             </section>
         );
     }
